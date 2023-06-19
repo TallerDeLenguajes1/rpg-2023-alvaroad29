ec3caa22587703144fdbb4161bcd0d2b17683d66
Alvaro Diaz <alvaroad29012001@gmail.com>
2023-06-19 19:18:10 -0300

    Hasta verificar al comienzo

--- Personaje.cs
+++ Personaje.cs
@@ -1,12 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+
 namespace EspacioPersonaje
 {
-    public enum Tipo{humano,otros};
-    public class Personaje
+    public enum Tipo{humano,deidad,hada,ogro,nigromante,mago};
+    
+    public class Personaje  //clase que contiene los datos y caracteristicas del personaje
     {
         //datos
         private Tipo tipo;
-        private string nombre;
-        private string apodo;
+        private string? nombre;
+        private string? apodo;
         private DateTime fechaNac;
         private int edad;
 
@@ -16,6 +22,7 @@ namespace EspacioPersonaje
         public string Apodo { get => apodo; set => apodo = value; }
         public DateTime FechaNac { get => fechaNac; set => fechaNac = value; }
         public int Edad { get => edad; set => edad = value; }
+
 
         //caracteristicas
         private int velocidad;
@@ -32,5 +39,85 @@ namespace EspacioPersonaje
         public int Nivel { get => nivel; set => nivel = value; }
         public int Armadura { get => armadura; set => armadura = value; }
         public int Salud { get => salud; set => salud = value; }
+
+        //metodos
+        public void mostrarPersonaje()
+        {
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Apodo: {Apodo}");
+            Console.WriteLine($"Tipo: {Tipo}");
+            Console.WriteLine($"Fecha de nacimiento: {FechaNac.ToShortDateString()}");
+            Console.WriteLine($"Edad: {Edad}");
+            Console.WriteLine($"Velocidad: {Velocidad}");
+            Console.WriteLine($"Destreza: {Destreza}");
+            Console.WriteLine($"Fuerza: {Fuerza}");
+            Console.WriteLine($"Nivel: {Nivel}");
+            Console.WriteLine($"Armadura: {Armadura}");
+            Console.WriteLine($"Salud: {Salud}");
+        }
+    }
+
+    public class FabricaDePersonajes //clase que genera personajes aleatorios
+    {
+        string[] nombres = { "Thaus", "Racid", "Hyr", "Red", "Flint", "Cecile" };
+        string[] apodos = { "Ace", "Shadow", "Raptor", "Luna", "Blaze", "Spike" };
+        public Personaje generarPersonaje() //metodo que retorna el personaje con los datos cargados
+        {
+            Personaje personaje = new Personaje(); //instancio el personaje
+            Random aleatorio = new Random();
+
+            //generar datos
+            personaje.Tipo = (Tipo)aleatorio.Next(0,Enum.GetValues(typeof(Tipo)).Length); //devuelve un array con los enum y obtengo su longitud
+            personaje.Nombre = nombres[aleatorio.Next(nombres.Length)];
+            personaje.Apodo = apodos[aleatorio.Next(apodos.Length)];
+            personaje.Edad = aleatorio.Next(0,300); //genero aleatoriamente la edad
+            personaje.FechaNac = DateTime.Now.AddYears(-personaje.Edad); //de la fecha actual resto la edad para pbtener la fecha de nacimiento
+
+            //generar caracterisitcas
+            personaje.Velocidad = aleatorio.Next(1,11);//indInferior,indSuperior + 1
+            personaje.Destreza = aleatorio.Next(1,6);
+            personaje.Fuerza = aleatorio.Next(1,11);
+            personaje.Nivel = aleatorio.Next(1,11);
+            personaje.Armadura = aleatorio.Next(1,11);
+            personaje.Salud = 100;
+
+            return personaje;
+        }
+    }
+
+    public class PersonajesJson
+    {
+        //guarda(lista,nombreArichivo)
+        public  void GuardarPersonajes(List<Personaje> listaDePersonajes,string nombreArchivo)
+        {
+            //SERIALIZA PARA GUARDARLO EN UN ARCHIVO JSON
+
+            string Personajesjson = JsonSerializer.Serialize(listaDePersonajes); //mando objeto y lo convierte a string con un formato json
+            File.WriteAllText(nombreArchivo,Personajesjson); //escribe y pisa lo anterior?
+        }
+
+        //leer 
+        public List<Personaje> LeerPersonajes(string nombreArchivo)
+        {
+            //DESERIALIZA  un archivo JSON a un objeto
+            //es importante q el nombre de las propiedades de la clase tienen q ser iguales a las etiquetas de json
+            if (File.Exists(nombreArchivo))
+            {
+                string jsonString = File.ReadAllText(nombreArchivo); //lee el json y lo guardo en un string
+                List<Personaje> listPer = JsonSerializer.Deserialize<List<Personaje>>(jsonString); //<tipo de dato>(variable donde esta la informacion (string)), pasa el string a una lista
+                return listPer;
+            }
+            else
+            {
+                System.Console.WriteLine($"El archivo {nombreArchivo} NO EXISTE");
+                return null;
+            }
+        }
+        //existe()
+        public bool Existe(string nombreArchivo)
+        {
+            //VERIFICO QUE EXISTA EL ARCHIVO
+            return File.Exists(nombreArchivo) && new FileInfo(nombreArchivo).Length > 0;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,52 @@
 ﻿using EspacioPersonaje;
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
+PersonajesJson guardarPersonajes = new PersonajesJson(); 
+string nombreArchivo = "Personajes.json";
+List<Personaje> listaPersonajes; 
+if (guardarPersonajes.Existe(nombreArchivo)) //verifico si el archivo existe y tiene datos
+{
+    //si existe, cargo una lista con los datos del archivo
+    System.Console.WriteLine("----------------------EXISTE------------------------");
+    listaPersonajes = guardarPersonajes.LeerPersonajes(nombreArchivo);
+}
+else
+{
+    System.Console.WriteLine("----------------------NO EXISTE------------------------");
+    //si no existe, genero 10 personajes y los guardo
+    FabricaDePersonajes fp = new FabricaDePersonajes();
+    Personaje nuevo;
+    listaPersonajes = new List<Personaje>();
+    for (int i = 0; i < 10; i++)
+    {
+        nuevo = fp.generarPersonaje();
+        listaPersonajes.Add(nuevo);
+    }
+    guardarPersonajes.GuardarPersonajes(listaPersonajes,nombreArchivo);
+}
+
+mostrarPersonajes(listaPersonajes);
+
+
+
+
+
+
+
+
+
+
+
+/*----------------- METODOS -------------------*/
+
+static void mostrarPersonajes(List<Personaje> lista)
+{
+    foreach (var item in lista)
+    {
+        item.mostrarPersonaje();
+        System.Console.WriteLine("#############");
+    }
+}
